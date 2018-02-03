a6262913a02b34715ad58ec3b7aae5dab82ec514
duonghan <20155296@student.hust.edu.vn>
2018-02-03 08:05:15 +0700

    change props in Square class

--- src/square.js
+++ src/square.js
@@ -3,13 +3,15 @@ import React from 'react';
 class Square extends React.Component {
     constructor(props){
         super(props);
-        this.value = props.value;
+        this.state = {
+            value: null
+        }
     }
 
     render() {
         return (
             <button className="square">
-                <img src={'./images/pokemon_'+ this.value + '.png'} alt={'image '+ this.value} />
+                <img src={'./images/pokemon_'+ this.props.value + '.png'} alt={'image '+ this.props.value} />
             </button>
         );
     }
