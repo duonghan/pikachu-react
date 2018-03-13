662a5a8aa81086f0e7167a1ca833ec9fee2192d4
duonghan <20155296@student.hust.edu.vn>
2018-03-13 22:40:19 +0700

    update image extension type

--- src/components/square.js
+++ src/components/square.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
                 style={
                     this.props.selected ? {border: '3px solid crimson'}: null
                 }>
-                <img src={'./images/pokemon_'+ this.props.value + '.jpg'} />
+                <img src={'./images/pokemon_'+ this.props.value + '.png'} />
             </button>
         );
     }
