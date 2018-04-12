c920e4ff8bb1b4e1270616e525fdd1a63923b1b7
duonghan <20155296@student.hust.edu.vn>
2018-04-12 22:57:49 +0700

    update isExist() method

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Board from './board';
-import { getBoard, reloadBoard } from '../functions/generator';
+import { getBoard, reloadBoard, tmpBoard } from '../functions/generator';
 
 const row = 7;
 const col = 14;
@@ -15,7 +15,8 @@ class Game extends React.Component {
         super(props);
 
         this.state = {
-            items : getBoard(row,col,amount),
+            // items : getBoard(row,col,amount),
+            items : tmpBoard(),
             score: 0,
             square1: null,
             square2: null,
@@ -30,7 +31,7 @@ class Game extends React.Component {
     //     debugger;
     // }
     componentDidMount () {
-        if(!this.isExist) this.reloadHandler;
+        // if(!this.isExist()) this.reloadHandler();
     }
     // componentWillReceiveProps() {
     //     debugger;
@@ -44,6 +45,8 @@ class Game extends React.Component {
     // }
 
     componentDidUpdate(prevProps, prevState) {
+
+        if(!this.isExist()) this.reloadHandler();
 
         //Vong4
         if(this.doneLine){
@@ -431,22 +434,68 @@ class Game extends React.Component {
         return false;
     };
 
+    /**
+     * Kiem tra kha nang con co the an diem cua bang pokemon
+     */
     isExist(){
-        let tmp = this.state.items.slice();
-        let list = [];
+        let tmp = this.state.items.slice(); //ban sao cua 
+        let list = []; //mảng của cách 1
+        let listPosItem = new Array(amount+1); // mảng 2 chiều lưu vị trí theo từng pokemon, index là giá trị của pokemon
+        let i,j,k; //các iterator
 
-        for(let i = 1; i<= row; i++)
-            for(let j=1; j<= col; j++){
+        //Cach 1: brute-force - vét cạn
+        // for(i = 1; i <= row; i++)
+        //     for(j = 1; j <= col; j++){
+        //         if(tmp[i][j] === 0) continue;
+        //         list.push({x: i, y: j});
+        //     }
+        
+        // list.map((item, index, list)=>{
+        //     list.map(pair =>{
+        //         if(this.isPair(item, pair))
+        //             return true;
+        //     });
+        // })
+
+        //Cach 2: bubble - nổi bọt : tương tự cách 1 
+        // for(i = 0; i < list.length; i++)
+        //     for(j = i+1; j < list.length; j++){
+        //         if(this.isPair(list[i], list[j]))
+        //             return true;
+        //     }
+
+        //Cách 3: kiểm tra điều kiện của các pokemon cùng loại
+        //Khởi tạo mảng 2 chiều
+        for(i = 1; i < listPosItem.length; i++)
+            listPosItem[i] = new Array();
+
+        //duyệt các phần tử trong bảng game và lưu vị trí theo từng pokemon
+        for(i = 1; i <= row; i++){
+            for(j = 1; j <= col; j++){
                 if(tmp[i][j] === 0) continue;
-                list.push({x: i, y: j});
-            }
 
-        list.map((item, index, list)=>{
-            list.map(pair =>{
-                if(this.isPair(item, pair))
-                    return true;
-            });
-        })
+                k = tmp[i][j];
+                listPosItem[k].push({x: i, y: j});
+            }
+        }
+            
+
+        //duyệt từng pokemon xem các vị trí của nó trên board có thể ăn được hay không
+        for(i = 1; i <= listPosItem.length; i++){
+            //Case 1: 0 item
+            if(!listPosItem[i] || listPosItem[i].length === 0) continue;
+
+            //Case 2: 2, 4, 6... items
+            for(j = 0; j < listPosItem[i].length ; j++){
+                for(k = j+1; k < listPosItem[i].length; k++){
+                    if(this.isPair(listPosItem[i][j], listPosItem[i][k])) return true;
+                }
+            }
+        }
+
+        //Cách 4: duyệt theo các hướng có thể ăn của từng item
+        //ongoing....
+            
 
         return false;
         
